e16f5ed4a0b2d9bf3527bb096073a281253b7b1c
Zuhao Wan <wanzuhao@gmail.com>
2013-07-10 14:15:52 +0800

    Use attr_accessor and ActiveModel validation

--- lib/plotrb/data.rb
+++ lib/plotrb/data.rb
@@ -7,6 +7,9 @@ module Plotrb
   # See {https://github.com/trifacta/vega/wiki/Data}
   class Data
 
+    include ::Plotrb::Validators
+    include ActiveModel::Validations
+
     attr_accessor :name, :format, :values, :source, :url, :transform
 
     def initialize(args={})
@@ -18,115 +21,63 @@ module Plotrb
       @transform  = args[:transform]
     end
 
-    # @param name [#to_s] unique name of the data set
-    def name=(name)
-      @name = name.to_s
-      if @name.nil? || @name.empty?
-        raise ::Plotrb::InvalidInputError
+    class UrlValidator < ActiveModel::EachValidator
+      def validate_each(record, attribute, value)
+        record.errors.add(attribute, 'invalid url') unless valid_url?(value)
+      end
+
+      def valid_url?(url)
+        URI.parse(url)
+      rescue URI::InvalidURIError
+        false
       end
     end
+
+    class TransformValidator < ActiveModel::EachValidator
+      def validate_each(record, attribute, value)
+        record.errors.add(attribute, 'invalid viewport') unless
+            ::Plotrb::Validators::array_of_Transform?(value)
+      end
+    end
+
+    class FormatValidator < ActiveModel::EachValidator
+      def validate_each(record, attribute, value)
+        record.errors.add(attribute, 'invalid format') unless
+            valid_format_key?(value) && valid_format_value?(value)
+      end
+
+      def valid_format_key?(format)
+        format.is_a?(Hash) && [:json, :csv, :tsv].include?(format[:type])
+      end
+
+      def valid_format_value?(format)
+        valid = true
+        if format[:parse]
+          format[:parse].each do |_, v|
+            valid = false unless [:number, :boolean, :date].include?(v)
+          end
+        end
+        valid
+      end
+    end
+
+    validates :name, presence: true, length: { minimum: 1 }
+    validates :source, allow_nil: true, length: { minimum: 1 }
+    validates :url, allow_nil: true, url: true
+    validates :transform, allow_nil: true, transform: true
+    validates :format, allow_nil: true, format: true
+
+    # @param name [#to_s] unique name of the data set
 
     # @param format [Hash] the format for the data file
-    def format=(format)
-      if format.is_a?(Hash) && format[:type] &&
-          [:json, :csv, :tsv].include?(format[:type]) &&
-          self.send("valid_#{format[:type].to_s}_format?", format)
-        @format = format
-      else
-        raise ::Plotrb::InvalidInputError
-      end
-    end
 
     # @param values [Hash] the actual data set
-    def values=(values)
-      @values = Yajl::Parser.parse(values)
-    rescue Yajl::ParseError
-      raise ::Plotrb::InvalidInputError
-    end
 
     # @param source [String] the name of another data set to us as source
-    def source=(source)
-      if valid_source?(source)
-        @source = source
-      else
-        raise ::Plotrb::InvalidInputError
-      end
-    end
 
     # @param url [String] the url from which to load the data set
-    def url=(url)
-      u = URI.parse(url)
-      if u
-        @url = u.to_s
-      end
-    rescue URI::InvalidURIError
-      raise ::Plotrb::InvalidInputError
-    end
 
     # @param transform [Array<Transform>] an array of transform definitions
-    def transform=(transform)
-      if valid_transform?(transform)
-        @transform = transform
-      else
-        raise ::Plotrb::InvalidInputError
-      end
-    end
-
-  private
-
-    # @param source [String] the name of another data set to us as source
-    def valid_source?(source)
-      return true unless source
-      #TODO: check if source data set exists.
-    end
-
-    # @param format [Hash] the format object
-    def valid_json_format?(format)
-      valid = true
-      if format[:parse]
-        format[:parse].each do |_, v|
-          valid &= [:number, :boolean, :date].include?(v)
-        end
-      end
-      if format[:property]
-        valid &= format[:property].is_a?(String)
-      end
-      valid
-    rescue
-      false
-    end
-
-    # (see #valid_json_format?)
-    def valid_csv_format?(format)
-      valid = true
-      if format[:parse]
-        format[:parse].each do |_, v|
-          valid &= [:number, :boolean, :date].include?(v)
-        end
-      end
-      valid
-    rescue
-      false
-    end
-
-    # (see #valid_json_format?)
-    def valid_tsv_format?(format)
-      valid = true
-      if format[:parse]
-        format[:parse].each do |_, v|
-          valid &= [:number, :boolean, :date].include?(v)
-        end
-      end
-      valid
-    rescue
-      false
-    end
-
-    # @param transform [nil, Array<Transform>] an array of transform definitions
-    def valid_transform?(transform)
-      transform.is_a?(Array) &&
-          transform.all? { |t| t.is_a?(::Plotrb::Transform) }
-    end
 
   end
 
--- lib/plotrb/scales.rb
+++ lib/plotrb/scales.rb
@@ -5,159 +5,65 @@ module Plotrb
   # See {https://github.com/trifacta/vega/wiki/Scales}
   class Scale
 
-    def initialize
+    include ActiveModel::Validations
 
-    end
+    attr_accessor :name, :type, :domain, :domain_min, :domain_max, :range,
+                  :range_min, :range_max, :reverse, :round, :points, :clamp,
+                  :nice, :exponent, :zero, :data, :field
 
-    # Common Scale Properties
-
-    attr_accessor :name, :type, :domain, :domain_min, :domain_max,
-                  :range, :range_min, :range_max, :reverse, :round
-
-    # @param name [String] unique name of the scale
-    def name=(name)
-      @name = name.to_s
-      raise ::Plotrb::InvalidInputError if @name.empty?
-    end
-
-    # @param type [Symbol, String] the type of scale
-    def type=(type)
-      if valid_type?(type)
-        @type = type.to_sym
-      else
-        raise ::Plotrb::InvalidInputError
+    class DomainValidator < ActiveModel::EachValidator
+      def validate_each(record, attribute, value)
+        record.errors.add(attribute, 'invalid domain')
       end
     end
+
+    validates :name, presence: true, length: { minimum: 1 }
+    validates :type, allow_nil: true,
+              inclusion: { in: %i(linear ordinal time utc log pow sqrt quantile
+                                  quantize threshold) }
+
 
     # @param domain [Array(Numeric, Numeric), Array, String] the domain of the
     #   scale, representing the set of data values
-    def domain=(domain)
-      if valid_domain?(domain)
-        @domain = domain
-      else
-        raise ::Plotrb::InvalidInputError
-      end
-    end
 
     # @param domain_min [Numeric, String] the minimum value in the scale domain
-    def domain_min=(domain_min)
-      @domain_min = domain_min
-    end
 
     # @param domain_max [Numeric, String] the maximum value in the scale domain
-    def domain_max=(domain_max)
-      @domain_max = domain_max
-    end
 
     # @param range [Array(Numeric, Numeric), Array, String] the range of the
     #   scale, representing the set of visual values
-    def range=(range)
-      if valid_range?(range)
-        @range = range
-      else
-        raise ::Plotrb::InvalidInputError
-      end
-    end
 
     # @param range_min [Numeric, String] the minimum value in the scale range
-    def range_min=(range_min)
-      @range_min = range_min
-    end
 
     # @param range_max [Numeric, String] the maximum value in the scale range
-    def range_max=(range_max)
-      @range_max = range_max
-    end
 
     # @param reverse [Boolean] whether flips the scale range
-    def reverse=(reverse)
-      @reverse = reverse
-    end
 
     # @param round [Boolean] whether rounds numeric output values to integers
-    def round=(round)
-      @round = round
-    end
 
     # Ordinal Scale Properties
 
-    attr_accessor :points
-
     # @param points [Boolean] whether distributes the ordinal values over a
     #   quantitative range at uniformly spaced points or bands
-    def points=(points)
-      @points = points
-    end
 
     # Time Scale Properties
 
-    attr_accessor :clamp, :nice
-
     # @param clamp [Boolean] whether clamps values that exceed the data domain
     #   to either to minimum or maximum range value
-    def clamp=(clamp)
-      @clamp = clamp
-    end
 
     # @param nice [Symbol, Boolean, nil] scale domain ina more human-friendly value range
-    def nice=(nice)
-      if valid_nice?(nice)
-        @nice = nice
-      else
-        raise ::Plotrb::InvalidInputError
-      end
-    end
 
     # Quantitative Scale Properties
 
-    attr_accessor :exponent, :zero
-
     # @param exponent [Numeric] the exponent of the scale transformation
-    def exponent=(exponent)
-      @exponent = exponent
-    end
 
     # @param zero [Boolean] whether zero baseline value is included
-    def zero=(zero)
-      @zero = zero
-    end
 
     # Scale Domains
 
-    attr_accessor :data, :field
-
     # @param data [String] the name of the data set containing domain values
-    def data=(data)
-      @data =data
-    end
 
     # @param field [String, Array<String>] reference to the desired data fields
-    def field=(field)
-      @field = field
-    end
-
-  private
-
-    def valid_type?(type)
-      [:linear, :ordinal, :time, :utc, :log, :pow, :sqrt, :quantile,
-       :quantize, :threshold].include?(type.to_sym)
-    end
-
-    def valid_domain?(domain)
-
-    end
-
-    def valid_range?(range)
-
-    end
-
-    def valid_nice?(nice)
-      if [:time, :utc].include?(@type)
-        [:second, :minute, :hour, :day, :week, :month, :year].include?(nice)
-      else
-        [true, false].include?(nice)
-      end
-    end
 
   end
 
