a8edcee45ca201c51093c4d016efb0722f950184
Zuhao Wan <wanzuhao@gmail.com>
2013-07-12 16:54:27 +0800

    Make sure load internals module first.

--- lib/plotrb.rb
+++ lib/plotrb.rb
@@ -4,14 +4,16 @@ require 'uri'
 require 'hashie'
 require 'active_model'
 
+require_relative 'plotrb/internals'
+require_relative 'plotrb/validators'
+
 require_relative 'plotrb/visualization'
 require_relative 'plotrb/errors'
 require_relative 'plotrb/data'
 require_relative 'plotrb/transforms'
 require_relative 'plotrb/scales'
 require_relative 'plotrb/marks'
-require_relative 'plotrb/validators'
-require_relative 'plotrb/internals'
+require_relative 'plotrb/axes'
 
 module Plotrb
 
