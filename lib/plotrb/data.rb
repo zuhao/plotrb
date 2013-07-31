69806ca70f3d9786e2fd1f671b7eb330bc37f057
Zuhao Wan <wanzuhao@gmail.com>
2013-07-31 14:48:32 +0800

    Add private Format class to Data.

--- lib/plotrb/data.rb
+++ lib/plotrb/data.rb
@@ -154,6 +154,134 @@ module Plotrb
       raise ArgumentError
     end
 
+    class Format
+
+      include ::Plotrb::Base
+
+      add_attributes :format
+
+      def initialize(format, &block)
+        case format
+          when :json
+            add_attributes(:parse, :property)
+          when :csv, :tsv
+            add_attributes(:parse)
+          when :topojson
+            add_attributes(:feature, :mesh)
+          when :treejson
+            add_attributes(:children, :parse)
+          else
+            raise ArgumentError
+        end
+        @format = format
+        self.instance_eval(&block) if block_given?
+        self
+      end
+
+      def format(*args, &block)
+        case args.size
+          when 0
+            @format
+          when 1
+            initialize(args[0], &block)
+            self
+          else
+            raise ArgumentError
+        end
+      end
+
+      def parse(*args, &block)
+        case args.size
+          when 0
+            @parse
+          when 1
+            # e.g parse('some_field' => :date, 'some_other_field' => :number)
+            valid_type = %i(number boolean date)
+            raise NoMethodError unless self.attributes.include?(:parse)
+            raise ArgumentError unless args[0].is_a?(Hash) &&
+                (args[0].values - valid_type).empty?
+            @parse ||= {}
+            @parse.merge!(args[0])
+            self.instance_eval(&block) if block_given?
+            self
+          else
+            raise ArgumentError
+        end
+      end
+
+      def date(field, &block)
+        parse(field => :date, &block)
+        self
+      end
+
+      def number(field, &block)
+        parse(field => :number, &block)
+        self
+      end
+
+      def boolean(field, &block)
+        parse(field => :boolean, &block)
+        self
+      end
+
+      def property(*args, &block)
+        raise NoMethodError unless self.attributes.include?(:property)
+        case args.size
+          when 0
+            @property
+          when 1
+            @property = args[0]
+            self.instance_eval(&block) if block_given?
+            self
+          else
+            raise ArgumentError
+        end
+      end
+
+      def feature(*args, &block)
+        raise NoMethodError unless self.attributes.include?(:feature)
+        case args.size
+          when 0
+            @feature
+          when 1
+            @feature = args[0]
+            self.instance_eval(&block) if block_given?
+            self
+          else
+            raise ArgumentError
+        end
+      end
+
+      def mesh(*args, &block)
+        raise NoMethodError unless self.attributes.include?(:mesh)
+        case args.size
+          when 0
+            @mesh
+          when 1
+            @mesh = args[0]
+            self.instance_eval(&block) if block_given?
+            self
+          else
+            raise ArgumentError
+        end
+      end
+
+      def children(*args, &block)
+        raise NoMethodError unless self.attributes.include?(:children)
+        case args.size
+          when 0
+            @children
+          when 1
+            @children = args[0]
+            self.instance_eval(&block) if block_given?
+            self
+          else
+            raise ArgumentError
+        end
+      end
+
+    end
+
   end
 
 end
