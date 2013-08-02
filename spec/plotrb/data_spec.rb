1ca8c35ec3ac2e69d2fed54d6e269ae06af7a43e
Zuhao Wan <wanzuhao@gmail.com>
2013-08-02 14:09:56 +0800

    Implement and pass tests for Data class.

--- spec/plotrb/data_spec.rb
+++ spec/plotrb/data_spec.rb
@@ -1,238 +1,228 @@
 require_relative '../spec_helper'
 
-describe 'Data', :broken => true do
+describe 'Data' do
 
-  before(:each) do
-    @data = ::Plotrb::Data.new
-  end
+  subject { ::Plotrb::Data.new }
 
-  context 'when setting name' do
+  describe '#name' do
 
     it 'sets name if valid' do
-      @data.name = 'foo'
-      @data.name.should == 'foo'
+      subject.name = 'foo'
+      subject.name.should == 'foo'
     end
 
-    it 'raises error if name is nil' do
-      expect { @data.name = nil }.to raise_error ::Plotrb::InvalidInputError
+    it 'raises error if name is nil'
+
+    it 'raises error if name is not unique'
+
+  end
+
+  describe '#format' do
+
+    it 'sets format as a new Format instance' do
+      ::Plotrb::Data::Format.should_receive(:new).with(:foo)
+      subject.format(:foo)
     end
 
   end
 
-  context 'when setting format' do
+  describe '#values' do
 
-    it 'raises error if format is not a hash' do
-      expect { @data.format = :foo }.to raise_error ::Plotrb::InvalidInputError
+    it 'sets values if valid JSON is given' do
+      subject.values('{"foo":1, "bar":{"baz":2}}')
+      subject.values.should == '{"foo":1, "bar":{"baz":2}}'
     end
 
-    it 'raises error if format does not has type' do
-      expect { @data.format = {:foo => :bar} }.
-          to raise_error ::Plotrb::InvalidInputError
+    it 'sets values if array is given' do
+      subject.values([1,2,3,4])
+      subject.values.should match_array([1,2,3,4])
     end
 
-    it 'raises error if format type is not json, csv, or tsv' do
-      expect { @data.format = {:type => :foo} }.
-          to raise_error ::Plotrb::InvalidInputError
+    it 'sets values if hash is given' do
+      subject.values(foo: 1, bar: 2)
+      subject.values.should == {foo: 1, bar: 2}
+    end
+
+    it 'raises error if values are invalid JSON' do
+      expect { subject.values("{foo:1, 'bar':}") }.to raise_error ArgumentError
+    end
+
+  end
+
+  describe '#source' do
+
+    it 'sets source if valid' do
+      subject.source('foo')
+      subject.source.should == 'foo'
+    end
+
+    it 'raises error if source does not exist'
+
+    it 'validates existing source'
+
+  end
+
+  describe '#url' do
+
+    it 'sets valid absolute url' do
+      subject.url('http://foo.com/bar')
+      subject.url.should == 'http://foo.com/bar'
+    end
+
+    it 'sets valid relative url' do
+      subject.url('data/bar.json')
+      subject.url.should == 'data/bar.json'
+    end
+
+    it 'raises error when url is invalid' do
+      expect { subject.url('http://foo/#-|r|$@') }.to raise_error ArgumentError
+    end
+
+  end
+
+  describe '#file' do
+
+    it 'sets url if file exists'
+
+    it 'raises error is file does not exist'
+
+  end
+
+  describe '#transform' do
+
+    class Bar; end
+
+    let(:foo) { ::Plotrb::Transform.new }
+    let(:bar) { Bar.new }
+
+    it 'sets transform if a transform object is given' do
+      subject.transform(foo)
+      subject.transform.should == [foo]
+    end
+
+    it 'sets transform if multiple transforms are given' do
+      subject.transform(foo, foo)
+      subject.transform.should == [foo, foo]
+    end
+
+    it 'raises error if array contains non-transforms' do
+      expect { subject.transform(foo, bar) }.to raise_error ArgumentError
+    end
+
+  end
+
+  describe '#method_missing' do
+
+    it 'sets format via as_foo' do
+      subject.should_receive(:format).with(:csv)
+      subject.as_csv
+    end
+
+  end
+
+  describe 'Format' do
+
+    it 'raises error if format type is not recognized' do
+      expect { ::Plotrb::Data::Format.new(:foo) }.to raise_error ArgumentError
     end
 
     context 'json' do
 
-      let(:valid_json) { {
-          :type => :json,
-          :parse => {:modified_on => :date},
-          :property => 'values.features'
-      } }
-      let(:invalid_json) { {:foo => :bar} }
+      subject { ::Plotrb::Data::Format.new(:json) }
 
-      it 'validates JSON format' do
-        @data.should_receive(:valid_json_format?).with(valid_json).
-            and_return(true)
-        @data.format = valid_json
+      it 'has parse and property attributes' do
+        subject.attributes.should match_array([:format, :parse, :property])
       end
 
-      it 'sets JSON format if valid' do
-        @data.format = valid_json
-        @data.format.should == valid_json
+      describe '#parse' do
+
+        it 'sets parse if valid hash is given' do
+          subject.parse('foo' => :number, 'bar' => :date)
+          subject.parse.should == {'foo' => :number, 'bar' => :date }
+        end
+
+        it 'raises error if parse object has unknown data type' do
+          expect { subject.parse('foo' => :bar) }.to raise_error ArgumentError
+        end
+
       end
 
-      it 'raises error if invalid JSON format' do
-        @data.stub(:valid_json_format?).and_return(false)
-        expect { @data.format = invalid_json }.
-            to raise_error ::Plotrb::InvalidInputError
+      describe '#as_date' do
+
+        it 'parses the field as date' do
+          subject.as_date('foo')
+          subject.parse['foo'].should == :date
+        end
+
       end
 
-      it 'invalidates if parse object has unknown data type' do
-        @data.send(:valid_json_format?, {:parse => {:foo => :bar }}).
-            should be_false
+      describe '#as_boolean' do
+
+        it 'parses the field as boolean' do
+          subject.as_boolean('foo')
+          subject.parse['foo'].should == :boolean
+        end
+
       end
 
-      it 'invalidates if parse object is not a hash' do
-        @data.send(:valid_json_format?, {:parse => :foo}).should be_false
+      describe '#as_number' do
+
+        it 'parses the field as number' do
+          subject.as_number('foo')
+          subject.parse['foo'].should == :number
+        end
+
       end
 
-      it 'invalidates if property is not a string' do
-        @data.send(:valid_json_format?, {:property => [:foo]}).should be_false
+      describe '#property' do
+
+        it 'sets the property' do
+          subject.property('values.features')
+          subject.property.should == 'values.features'
+        end
+
       end
 
     end
 
     context 'csv' do
 
-      let(:valid_csv) { {
-          :type => :csv,
-          :parse => {:modified_on => :date},
-          :property => 'values.features'
-      } }
-      let(:invalid_csv) { {:foo => :bar}}
+      subject { ::Plotrb::Data::Format.new(:csv) }
 
-      it 'validates csv format' do
-        @data.should_receive(:valid_csv_format?).with(valid_csv).
-            and_return(true)
-        @data.format = valid_csv
-      end
-
-      it 'sets csv format if valid' do
-        @data.format = valid_csv
-        @data.format.should == valid_csv
-      end
-
-      it 'raises error if invalid csv format' do
-        @data.stub(:valid_csv_format?).and_return(false)
-        expect { @data.format = invalid_csv }.
-            to raise_error ::Plotrb::InvalidInputError
-      end
-
-      it 'invalidates if parse object has unknown data type' do
-        @data.send(:valid_csv_format?, {:parse => {:foo => :bar }}).
-            should be_false
-      end
-
-      it 'invalidates if parse object is not a hash' do
-        @data.send(:valid_csv_format?, {:parse => :foo}).should be_false
+      it 'has parse attribute' do
+        subject.attributes.should match_array([:format, :parse])
       end
 
     end
 
     context 'tsv' do
 
-      let(:valid_tsv) { {
-          :type => :tsv,
-          :parse => {:modified_on => :date},
-          :property => 'values.features'
-      } }
-      let(:invalid_tsv) { {:foo => :bar}}
+      subject { ::Plotrb::Data::Format.new(:tsv) }
 
-      it 'validates tsv format' do
-        @data.should_receive(:valid_tsv_format?).with(valid_tsv).
-            and_return(true)
-        @data.format = valid_tsv
-      end
-
-      it 'sets tsv format if valid' do
-        @data.format = valid_tsv
-        @data.format.should == valid_tsv
-      end
-
-      it 'raises error if invalid tsv format' do
-        @data.stub(:valid_tsv_format?).and_return(false)
-        expect { @data.format = invalid_tsv }.
-            to raise_error ::Plotrb::InvalidInputError
-      end
-
-      it 'invalidates if parse object has unknown data type' do
-        @data.send(:valid_tsv_format?, {:parse => {:foo => :bar }}).
-            should be_false
-      end
-
-      it 'invalidates if parse object is not a hash' do
-        @data.send(:valid_tsv_format?, {:parse => :foo}).should be_false
+      it 'has parse attribute' do
+        subject.attributes.should match_array([:format, :parse])
       end
 
     end
 
-  end
+    context 'topojson' do
 
-  context 'when setting values' do
+      subject { ::Plotrb::Data::Format.new(:topojson) }
 
-    it 'sets values if valid JSON' do
-      @data.values = '{"foo":1, "bar":{"baz":2}}'
-      @data.values.should == {'foo'=>1, 'bar'=>{'baz'=>2}}
+      it 'has feature and mesh attribute' do
+        subject.attributes.should match_array([:format, :feature, :mesh])
+      end
+
     end
 
-    it 'raises error if values are invalid JSON' do
-      expect { @data.values = "{foo:1, 'bar':}" }.
-          to raise_error ::Plotrb::InvalidInputError
-    end
+    context 'treejson' do
 
-  end
+      subject { ::Plotrb::Data::Format.new(:treejson) }
 
-  context 'when setting source' do
+      it 'has parse and children attribute' do
+        subject.attributes.should match_array([:format, :parse, :children])
+      end
 
-    it 'sets source if valid' do
-      @data.stub(:valid_source?).and_return(true)
-      @data.source = 'foo'
-      @data.source.should == 'foo'
-    end
-
-    it 'raises error if source does not exist' do
-      @data.stub(:valid_source?).and_return(false)
-      expect { @data.source = 'foo' }.to raise_error ::Plotrb::InvalidInputError
-    end
-
-    it 'validates existing source'
-
-    it 'validates nil source' do
-      @data.should_receive(:valid_source?).with(nil).and_return(true)
-      @data.source = nil
-    end
-
-  end
-
-  context 'when setting url' do
-
-    it 'sets valid absolute url' do
-      @data.url = 'http://foo.com/bar'
-      @data.url.should == 'http://foo.com/bar'
-    end
-
-    it 'sets valid relative url' do
-      @data.url = 'data/bar.json'
-      @data.url.should == 'data/bar.json'
-    end
-
-    it 'raises error when url is invalid' do
-      expect { @data.url = 'http://foo/#@-$|bar|$-@#' }.
-          to raise_error ::Plotrb::InvalidInputError
-    end
-
-  end
-
-  context 'when setting transform' do
-
-    class Baz; end
-
-    let(:foo) { ::Plotrb::Transform.stub(:new) }
-    let(:bar) { [:foo, Baz.new]}
-    let(:valid_transform) { [:foo, :foo] }
-
-    it 'invalidates if transforms are not in an array' do
-      @data.send(:valid_transform?, foo).should be_false
-    end
-
-    it 'invalidates if array contains non-transforms' do
-      @data.send(:valid_transform?, bar).should be_false
-    end
-
-    it 'sets transforms if valid' do
-      @data.stub(:valid_transform?).and_return(true)
-      @data.transform = valid_transform
-      @data.transform.should == valid_transform
-    end
-
-    it 'raises error if transforms are invalid' do
-      @data.stub(:valid_transform?).and_return(false)
-      expect { @data.transform = bar }.
-          to raise_error ::Plotrb::InvalidInputError
     end
 
   end
