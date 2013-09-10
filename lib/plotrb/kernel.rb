3c88fa062ee0331bf62e29aa07d6623e3e7ef189
Zuhao Wan <wanzuhao@gmail.com>
2013-09-10 16:35:45 +0800

    Decide to use method_missing for intialization shortcuts, and cache methods.

--- lib/plotrb/kernel.rb
+++ lib/plotrb/kernel.rb
@@ -3,8 +3,30 @@ module Plotrb
   # Kernel module includes most of the shortcuts used in Plotrb
   module Kernel
 
-    # Don't use method_missing although it will shorten the code considerably.
-    # Clarity is more important.
+    # a global space keeping track of all Data objects defined
+    def self.data
+      @data ||= []
+    end
+
+    # a global space keeping track of all Axis objects defined
+    def self.axes
+      @axes ||= []
+    end
+
+    # a global space keeping track of all Scale objects defined
+    def self.scales
+      @scales ||= []
+    end
+
+    # a global space keeping track of all Mark objects defined
+    def self.marks
+      @marks ||= []
+    end
+
+    # a global space keeping track of all Transform objects defined
+    def self.transforms
+      @transforms ||= []
+    end
 
     # Initialize ::Plotrb::Visualization object
 
@@ -18,184 +40,56 @@ module Plotrb
       ::Plotrb::Data.new(&block)
     end
 
-    # Initialize ::Plotrb::Axis objects
-
-    def x_axis(&block)
-      ::Plotrb::Axis.new(:x, &block)
+    def method_missing(method, *args, &block)
+      case method.to_s
+        when /^(\w+)_axis$/
+          # Initialize ::Plotrb::Axis objects
+          if ::Plotrb::Axis::TYPES.include?($1.to_sym)
+            cache_method($1, 'axis')
+            self.send(method)
+          else
+            super
+          end
+        when /^(\w+)_scale$/
+          # Initialize ::Plotrb::Scale objects
+          if ::Plotrb::Scale::TYPES.include?($1.to_sym)
+            cache_method($1, 'scale')
+            self.send(method)
+          else
+            super
+          end
+        when /^(\w+)_transform$/
+          # Initialize ::Plotrb::Transform objects
+          if ::Plotrb::Transform::TYPES.include?($1.to_sym)
+            cache_method($1, 'transform')
+            self.send(method)
+          else
+            super
+          end
+        when /^(\w+)_mark$/
+          # Initialize ::Plotrb::Mark objects
+          if ::Plotrb::Mark::TYPES.include?($1.to_sym)
+            cache_method($1, 'mark')
+            self.send(method)
+          else
+            super
+          end
+        else
+          super
+      end
     end
 
-    def y_axis(&block)
-      ::Plotrb::Axis.new(:y, &block)
-    end
+  protected
 
-    # Initialize ::Plotrb::Scale objects
-
-    def linear_scale(&block)
-      ::Plotrb::Scale.new(:linear, &block)
-    end
-
-    def log_scale(&block)
-      ::Plotrb::Scale.new(:log, &block)
-    end
-
-    def pow_scale(&block)
-      ::Plotrb::Scale.new(:pow, &block)
-    end
-
-    def sqrt_scale(&block)
-      ::Plotrb::Scale.new(:sqrt, &block)
-    end
-
-    def quantile_scale(&block)
-      ::Plotrb::Scale.new(:quantile, &block)
-    end
-
-    def quantize_scale(&block)
-      ::Plotrb::Scale.new(:quantize, &block)
-    end
-
-    def threshold_scale(&block)
-      ::Plotrb::Scale.new(:threshold, &block)
-    end
-
-    def ordinal_scale(&block)
-      ::Plotrb::Scale.new(:ordinal, &block)
-    end
-
-    def time_scale(&block)
-      ::Plotrb::Scale.new(:time, &block)
-    end
-
-    def utc_scale(&block)
-      ::Plotrb::Scale.new(:utc, &block)
-    end
-
-    # Initialize ::Plotrb::Transform objects
-
-    def array_transform(&block)
-      ::Plotrb::Transform.new(:array, &block)
-    end
-
-    def copy_transform(&block)
-      ::Plotrb::Transform.new(:copy, &block)
-    end
-
-    def cross_transform(&block)
-      ::Plotrb::Transform.new(:cross, &block)
-    end
-
-    def facet_transform(&block)
-      ::Plotrb::Transform.new(:facet, &block)
-    end
-
-    def filter_transform(&block)
-      ::Plotrb::Transform.new(:filter, &block)
-    end
-
-    def flatten_transform(&block)
-      ::Plotrb::Transform.new(:flatten, &block)
-    end
-
-    def fold_transform(&block)
-      ::Plotrb::Transform.new(:fold, &block)
-    end
-
-    def formula_transform(&block)
-      ::Plotrb::Transform.new(:formula, &block)
-    end
-
-    def slice_transform(&block)
-      ::Plotrb::Transform.new(:slice, &block)
-    end
-
-    def sort_transform(&block)
-      ::Plotrb::Transform.new(:sort, &block)
-    end
-
-    def stats_transform(&block)
-      ::Plotrb::Transform.new(:stats, &block)
-    end
-
-    def truncate_transform(&block)
-      ::Plotrb::Transform.new(:truncate, &block)
-    end
-
-    def unique_transform(&block)
-      ::Plotrb::Transform.new(:unique, &block)
-    end
-
-    def window_transform(&block)
-      ::Plotrb::Transform.new(:window, &block)
-    end
-
-    def zip_transform(&block)
-      ::Plotrb::Transform.new(:zip, &block)
-    end
-
-    def force_transform(&block)
-      ::Plotrb::Transform.new(:force, &block)
-    end
-
-    def geo_transform(&block)
-      ::Plotrb::Transform.new(:geo, &block)
-    end
-
-    def geopath_transform(&block)
-      ::Plotrb::Transform.new(:geopath, &block)
-    end
-
-    def link_transform(&block)
-      ::Plotrb::Transform.new(:link, &block)
-    end
-
-    def pie_transform(&block)
-      ::Plotrb::Transform.new(:pie, &block)
-    end
-
-    def stack_transform(&block)
-      ::Plotrb::Transform.new(:stack, &block)
-    end
-
-    def treemap_transform(&block)
-      ::Plotrb::Transform.new(:treemap, &block)
-    end
-
-    def wordcloud_transform(&block)
-      ::Plotrb::Transform.new(:wordcloud, &block)
-    end
-
-    # Initialize ::Plotrb::Mark objects
-
-    def rect_mark(&block)
-      ::Plotrb::Mark.new(:rect, &block)
-    end
-
-    def symbol_mark(&block)
-      ::Plotrb::Mark.new(:symbol, &block)
-    end
-
-    def path_mark(&block)
-      ::Plotrb::Mark.new(:path, &block)
-    end
-
-    def arc_mark(&block)
-      ::Plotrb::Mark.new(:arc, &block)
-    end
-
-    def area_mark(&block)
-      ::Plotrb::Mark.new(:area, &block)
-    end
-
-    def line_mark(&block)
-      ::Plotrb::Mark.new(:line, &block)
-    end
-
-    def image_mark(&block)
-      ::Plotrb::Mark.new(:image, &block)
-    end
-
-    def text_mark(&block)
-      ::Plotrb::Mark.new(:text, &block)
+    def cache_method(type, klass)
+      self.class.class_eval {
+        define_method("#{type}_#{klass}") do |&block|
+          # class names are constants
+          # create shortcut methods to initialize Plotrb objects
+          ::Kernel::const_get("::Plotrb::#{klass.capitalize}").
+              new(type.to_sym, &block)
+        end
+      }
     end
 
   end
