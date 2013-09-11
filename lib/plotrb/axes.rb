90a64f62d6de67853d32fca6b2b3c6ce893c6fdc
Zuhao Wan <wanzuhao@gmail.com>
2013-09-11 20:40:39 +0800

    Axis class adapts to #attribute_post_processing.

--- lib/plotrb/axes.rb
+++ lib/plotrb/axes.rb
@@ -52,247 +52,36 @@ module Plotrb
     #   @return [Boolean] whether gridlines should be created
     add_attributes :type, :scale, :orient, :format, :ticks, :values, :subdivide,
                   :tick_padding, :tick_size, :tick_size_major, :tick_size_minor,
-                  :tick_size_end, :offset, :properties, :title, :title_offset,
-                  :grid
+                  :tick_size_end, :offset, :layer, :properties, :title,
+                  :title_offset, :grid
 
     def initialize(type, &block)
       @type = type
-      ::Plotrb::Kernel.axes << self
+      define_single_val_attributes(:scale, :orient, :title, :title_offset,
+                                   :format, :ticks, :subdivide, :tick_padding,
+                                   :tick_size, :tick_size_major, :tick_size_end,
+                                   :tick_size_minor, :offset, :layer)
+      define_boolean_attribute(:grid)
+      define_multi_val_attributes(:values)
+      self.singleton_class.class_eval {
+        alias_method :from, :scale
+        alias_method :offset_title_by, :title_offset
+        alias_method :subdivide_by, :subdivide
+        alias_method :major_tick_size, :tick_size_major
+        alias_method :minor_tick_size, :tick_size_minor
+        alias_method :end_tick_size, :tick_size_end
+        alias_method :offset_by, :offset
+        alias_method :show_grid, :grid
+        alias_method :with_grid, :grid
+        alias_method :show_grid?, :grid?
+        alias_method :with_grid?, :grid?
+      }
       self.instance_eval(&block) if block_given?
+      ::Plotrb::Kernel.axes << self
       self
     end
 
-    def type(*args, &block)
-      case args.size
-        when 0
-          @type
-        when 1
-          @type = args[0]
-          self.instance_eval(&block) if block_given?
-          self
-        else
-          raise ArgumentError
-      end
-    end
-
-    def scale(*args, &block)
-      case args.size
-        when 0
-          @scale
-        when 1
-          scale = args[0]
-          @scale =
-              case scale
-                when ::Plotrb::Scale
-                  @scale = scale.name
-                when String
-                  @scale = scale
-                else
-                  raise ArgumentError
-              end
-          self.instance_eval(&block) if block_given?
-          self
-        else
-          raise ArgumentError
-      end
-    end
-    alias_method :from, :scale
-
-    def orient(*args, &block)
-      case args.size
-        when 0
-          @orient
-        when 1
-          @orient = args[0].to_sym
-          self.instance_eval(&block) if block_given?
-          self
-        else
-          raise ArgumentError
-      end
-    end
-
-    def title(*args, &block)
-      case args.size
-        when 0
-          @title
-        when 1
-          @title = args[0]
-          self.instance_eval(&block) if block_given?
-          self
-        when 2
-          @title, @title_offset = args[0], args[1]
-          self.instance_eval(&block) if block_given?
-          self
-        else
-          raise ArgumentError
-      end
-    end
-
-    def title_offset(*args, &block)
-      case args.size
-        when 0
-          @title_offset
-        when 1
-          @title_offset = args[0]
-          self.instance_eval(&block) if block_given?
-          self
-        else
-          raise ArgumentError
-      end
-    end
-    alias_method :offset_title_by, :title_offset
-
-    def format(*args, &block)
-      case args.size
-        when 0
-          @format
-        when 1
-          @format = format
-          self.instance_eval(&block) if block_given?
-          self
-        else
-          raise ArgumentError
-      end
-    end
-
-    def ticks(*args, &block)
-      case args.size
-        when 0
-          @ticks
-        when 1
-          @ticks = args[0].to_i
-          self.instance_eval(&block) if block_given?
-          self
-        else
-          raise ArgumentError
-      end
-    end
-
-    def values(*args, &block)
-      case args.size
-        when 0
-          @values
-        when 1 # eg. values([1,2,3,4])
-          @values = args[0]
-          self.instance_eval(&block) if block_given?
-          self
-        else # eg. values(1,2,3,4)
-          @values = args
-          self.instance_eval(&block) if block_given?
-          self
-      end
-    end
-
-    def subdivide(*args, &block)
-      case args.size
-        when 0
-          @subdivide
-        when 1
-          @subdivide = args[0].to_i
-          self.instance_eval(&block) if block_given?
-          self
-        else
-          raise ArgumentError
-      end
-    end
-    alias_method :subdivide_by, :subdivide
-
-    def tick_padding(*args, &block)
-      case args.size
-        when 0
-          @tick_padding
-        when 1
-          @tick_padding = args[0].to_i
-          self.instance_eval(&block) if block_given?
-          self
-        else
-          raise ArgumentError
-      end
-    end
-
-    def tick_size(*args, &block)
-      case args.size
-        when 0
-          @tick_size
-        when 1
-          @tick_size = args[0].to_i
-          self.instance_eval(&block) if block_given?
-          self
-        else
-          raise ArgumentError
-      end
-    end
-
-    def tick_size_major(*args, &block)
-      case args.size
-        when 0
-          @tick_size_major
-        when 1
-          @tick_size_major = args[0].to_i
-          self.instance_eval(&block) if block_given?
-          self
-        else
-          raise ArgumentError
-      end
-    end
-    alias_method :major_tick_size, :tick_size_major
-
-    def tick_size_minor(*args, &block)
-      case args.size
-        when 0
-          @tick_size_minor
-        when 1
-          @tick_size_minor = args[0].to_i
-          self.instance_eval(&block) if block_given?
-          self
-        else
-          raise ArgumentError
-      end
-    end
-    alias_method :minor_tick_size, :tick_size_minor
-
-    def tick_size_end(*args, &block)
-      case args.size
-        when 0
-          @tick_size_end
-        when 1
-          @tick_size_end = args[0].to_i
-          self.instance_eval(&block) if block_given?
-          self
-        else
-          raise ArgumentError
-      end
-    end
-    alias_method :end_tick_size, :tick_size_end
-
-    def offset(*args, &block)
-      case args.size
-        when 0
-          @offset
-        when 1
-          @offset = args[0].to_i
-          self.instance_eval(&block) if block_given?
-          self
-        else
-          raise ArgumentError
-      end
-    end
-    alias_method :offset_by, :offset
-
-    def layer(*args, &block)
-      case args.size
-        when 0
-          @layer
-        when 1
-          @layer = args[0].to_sym
-          self.instance_eval(&block) if block_given?
-          self
-        else
-          raise ArgumentError
-      end
-    end
-
-    def above(*args, &block)
+    def above(&block)
       @layer = :front
       self.instance_eval(&block) if block_given?
       self
@@ -312,19 +101,20 @@ module Plotrb
       @layer == :back
     end
 
-    def grid(&block)
-      @grid = true
-      self.instance_eval(&block) if block_given?
+    def no_grid(&block)
+      @grid = false
+      self.instance_eval(&block) if block
       self
     end
-    alias_method :show_grid, :grid
-    alias_method :with_grid, :grid
 
-    def grid?
-      @grid
+    def properties(element=nil, &block)
+      @properties ||= {}
+      return @properties unless element
+      @properties.merge!(
+          element.to_sym => ::Plotrb::Mark::MarkProperty.new(:text, &block)
+      )
+      self
     end
-    alias_method :show_grid?, :grid?
-    alias_method :with_grid?, :grid?
 
     def method_missing(method, *args, &block)
       case method.to_s
@@ -338,6 +128,74 @@ module Plotrb
           self.layer($1.to_sym, &block)
         else
           super
+      end
+    end
+
+  private
+
+    def attribute_post_processing
+      process_type
+      process_scale
+      process_orient
+      process_format
+      process_layer
+      process_properties
+    end
+
+    def process_type
+      unless TYPES.include?(@type)
+        raise ArgumentError, 'Invalid Axis type'
+      end
+    end
+
+    def process_scale
+      return unless @scale
+      case @scale
+        when String
+          unless ::Plotrb::Kernel.find_scale(@scale)
+            raise ArgumentError, 'Scale not found'
+          end
+        when ::Plotrb::Scale
+          @scale = @scale.name
+        else
+          raise ArgumentError, 'Unknown Scale'
+      end
+    end
+
+    def process_orient
+      return unless @orient
+      unless %i(top bottom left right).include?(@orient.to_sym)
+        raise ArgumentError, 'Invalid Axis orient'
+      end
+    end
+
+    def process_format
+      return unless @format
+      # D3's format specifier has general form:
+      # [​[fill]align][sign][symbol][0][width][,][.precision][type]
+      # the regex is taken from d3/src/format/format.js
+      re =
+        /(?:([^{])?([<>=^]))?([+\- ])?([$#])?(0)?(\d+)?(,)?(\.-?\d+)?([a-z%])?/i
+      @format = @format.to_s
+      if @format =~ re
+        if "#{$1}#{$2}#{$3}#{$4}#{$5}#{$6}#{$7}#{$8}#{$9}" != @format
+          raise ArgumentError, 'Invalid format specifier'
+        end
+      end
+    end
+
+    def process_layer
+      return unless @layer
+      unless %i(front back).include?(@layer.to_sym)
+        raise ArgumentError, 'Invalid Axis layer'
+      end
+    end
+
+    def process_properties
+      return unless @properties
+      valid_elements = %i(ticks major_ticks minor_ticks grid labels axis)
+      unless (@properties.keys - valid_elements).empty?
+        raise ArgumentError, 'Invalid property element'
       end
     end
 
